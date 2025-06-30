053ac559af8fd69389f0bf56a0a7115435990d22
Anil <117368200+urz-anil@users.noreply.github.com>
2025-06-30 18:25:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,13 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Anil Rachamalla | Software Developer',
   description: 'Personal portfolio of Anil Rachamalla, a Software Developer with a strong interest in cybersecurity and web development.',
-  metadataBase: new URL('https://anil-portfolio-tawny.vercel.app/'),
+  metadataBase: new URL('https://anil-portfolio-eta.vercel.app/'),
   openGraph: {
     title: 'Anil Rachamalla | Software Developer',
     description: 'Personal portfolio of Anil Rachamalla, a Software Developer with a strong interest in cybersecurity and web development.',
     type: 'website',
     locale: 'en_US',
-    url: 'https://anil-portfolio-tawny.vercel.app/',
+    url: 'https://anil-portfolio-eta.vercel.app/',
   },
 };
 
